a6507a3ec2b6dfc91e9b5557b339dc603a7d69ba
Tramy Nguyen <tramynn2@gmail.com>
2019-08-20 16:51:54 -0500

    completed app 6

---
+++ app-6/src/Todo.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class Todo extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            list: [],
+            userInput: ''
+        };
+        this.handleInput = this.handleInput.bind(this);
+        this.addTodo = this.addTodo.bind(this);
+    }
+
+    handleInput(value) {
+        this.setState({userInput: value.target.value});
+    }
+
+    addTodo() {
+        let list = this.state.list.map((element, index) => {
+            return element + index;
+        }); 
+
+        this.setState({
+            list: [...this.state.list, this.state.userInput],
+            userInput: '',
+        });
+    }
+
+    render() {
+
+        return(
+            <div>
+                <input value={this.state.userInput} placeholder="Enter a new task" onChange={this.handleInput} />
+                <button onClick={this.addTodo}>Add</button>
+                <br />
+                <span> ----- </span>
+                <br />
+                { this.state.list } 
+            </div>
+        )
+    }
+}
+
+export default Todo;
